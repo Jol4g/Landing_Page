b0479c5223671af5a80869cd6e5bc33b6fdae333
fedisarray <fedisarray@gmail.com>
2021-07-14 22:47:52 +0100

    page one finish

--- src/pages/page-one/index.js
+++ src/pages/page-one/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import {Navbar} from "../../components";
-
+import './style.css'
 export default () => {
     return (
-        <div>
+        <div className={'page-one'}>
             <Navbar/>
+            <div className={'container'}>
+                <span className={'pre-post'}>I'm a</span>
+                <br/>
+                <span className={'post'}>FULLSTACK <span className={'js-brand'}>JS</span></span>
+                <br/>
+                <span className={'post'}>DEVELOPER .</span>
+                <span className={'text-background'}>developer</span>
+            </div>
         </div>
     )
 }
